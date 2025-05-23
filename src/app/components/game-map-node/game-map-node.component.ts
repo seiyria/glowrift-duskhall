5672d5edbc51989ec24a2ed00bca5cb862571e3d
Kyle J. Kemp <kyle@seiyria.com>
2025-05-23 17:54:18 -0500

    add current position indicator

--- src/app/components/game-map-node/game-map-node.component.ts
+++ src/app/components/game-map-node/game-map-node.component.ts
@@ -1,7 +1,7 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { TippyDirective } from '@ngneat/helipopper';
 import { ShowIfOptionDirective } from '../../directives/option-hide.directive';
-import { showLocationMenu } from '../../helpers';
+import { isAtNode, showLocationMenu } from '../../helpers';
 import { WorldLocation } from '../../interfaces';
 import { AtlasImageComponent } from '../atlas-image/atlas-image.component';
 import { MarkerLocationClaimComponent } from '../marker-location-claim/marker-location-claim.component';
@@ -21,6 +21,8 @@ export class GameMapNodeComponent {
   public node = input.required<WorldLocation>();
   public x = input.required<number>();
   public y = input.required<number>();
+
+  public isAtThisNode = computed(() => isAtNode(this.node()));
 
   public investigateLocation() {
     showLocationMenu.set(this.node());
