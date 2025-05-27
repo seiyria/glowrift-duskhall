79de55f9f6ccb3270ca96c444180b7199f62baa5
Kyle J. Kemp <kyle@seiyria.com>
2025-05-27 07:19:13 -0500

    no notifications while minimized

--- src/app/helpers/notify.ts
+++ src/app/helpers/notify.ts
@@ -3,6 +3,10 @@ import { Subject } from 'rxjs';
 
 export const canSendNotifications = signal<boolean>(true);
 
+function isPageVisible(): boolean {
+  return !document.hidden;
+}
+
 type NotificationCategory = 'Error' | 'Success' | 'Travel' | 'LocationClaim';
 
 const notification = new Subject<{
@@ -13,16 +17,16 @@ const notification = new Subject<{
 export const notification$ = notification.asObservable();
 
 export function notify(message: string, category: NotificationCategory): void {
-  if (!canSendNotifications()) return;
+  if (!isPageVisible() || !canSendNotifications()) return;
   notification.next({ message, type: 'show', category });
 }
 
 export function notifyError(message: string): void {
-  if (!canSendNotifications()) return;
+  if (!isPageVisible() || !canSendNotifications()) return;
   notification.next({ message, type: 'error', category: 'Error' });
 }
 
 export function notifySuccess(message: string): void {
-  if (!canSendNotifications()) return;
+  if (!isPageVisible() || !canSendNotifications()) return;
   notification.next({ message, type: 'success', category: 'Success' });
 }
