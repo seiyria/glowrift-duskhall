e2ddcd65321e41acb0ef8f983b62ac546140bfab
Kyle J. Kemp <kyle@seiyria.com>
2025-06-04 07:53:41 -0500

    add color to currency markers

--- src/app/components/marker-currency/marker-currency.component.ts
+++ src/app/components/marker-currency/marker-currency.component.ts
@@ -1,4 +1,4 @@
-import { TitleCasePipe } from '@angular/common';
+import { NgClass, TitleCasePipe } from '@angular/common';
 import { Component, computed, input } from '@angular/core';
 import { ALL_ICONS } from '../../helpers';
 import { GameCurrency } from '../../interfaces';
@@ -26,9 +26,30 @@ const icons: Record<GameCurrency, keyof typeof ALL_ICONS> = {
   Mana: 'gameBallGlow',
 };
 
+const colors: Record<GameCurrency, string> = {
+  'Air Core': 'text-green-500',
+  'Air Crystal': 'text-green-500',
+  'Air Shard': 'text-green-500',
+  'Air Sliver': 'text-green-500',
+  'Earth Core': 'text-amber-700',
+  'Earth Crystal': 'text-amber-700',
+  'Earth Shard': 'text-amber-700',
+  'Earth Sliver': 'text-amber-700',
+  'Fire Core': 'text-red-600',
+  'Fire Crystal': 'text-red-600',
+  'Fire Shard': 'text-red-600',
+  'Fire Sliver': 'text-red-600',
+  'Ice Core': 'text-sky-500',
+  'Ice Crystal': 'text-sky-500',
+  'Ice Shard': 'text-sky-500',
+  'Ice Sliver': 'text-sky-500',
+  'Soul Essence': 'text-indigo-500',
+  Mana: 'text-fuchsia-500',
+};
+
 @Component({
   selector: 'app-marker-currency',
-  imports: [IconComponent, GameCurrencyPipe, TitleCasePipe],
+  imports: [IconComponent, GameCurrencyPipe, TitleCasePipe, NgClass],
   templateUrl: './marker-currency.component.html',
   styleUrl: './marker-currency.component.css',
 })
@@ -37,4 +58,5 @@ export class MarkerCurrencyComponent {
   public value = input.required<number>();
 
   public icon = computed(() => icons[this.currency()]);
+  public color = computed(() => `${colors[this.currency()]}`);
 }
